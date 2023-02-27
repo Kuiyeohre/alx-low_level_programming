cb82bfc20a6832447384c0af3b8cb9da244fbf1b
kuiyekoghene ohre <kuiyeohre@gmail.com>
2023-02-27 15:55:43 -0800

    my first commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabets
+ * return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char c;
+	
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+	putchar('\n');
+	return (0);
+}
+				
