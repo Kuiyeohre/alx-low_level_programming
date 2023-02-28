cc3cd9ef5ad2a668ef6a471840d6dfc7b28fcfd3
kuiyekoghene ohre <kuiyeohre@gmail.com>
2023-02-27 17:13:29 -0800

    my fifth commit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - entry point
+ * return: always 0 (success)
+ */
+
+int main(void)
+{
+	int k;
+for (k = '0'; k <= '9'; k++)
+		putchar(k);
+	putchar('\n');
+	return (0);
+}
