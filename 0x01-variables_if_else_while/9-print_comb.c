72b6a2c2e97d3ebe224ba7860e3dd203568c87a5
kuiyekoghene ohre <kuiyeohre@gmail.com>
2023-02-27 17:56:49 -0800

    my eighth commit

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * return: always 0 (success)
+ */
+
+int main(void)
+{
+	int d;
+	for (d = '0'; d <= '9'; d++)
+	{ putchar(d);
+		if (d != '9')
+		{putchar(',');
+			putchar(' ');}
+	}
+	putchar('\n');
+	return (0);
+}
