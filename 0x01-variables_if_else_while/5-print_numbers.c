a2895213b288f7d8fe5868be5d7703065f3c1f4c
kuiyekoghene ohre <kuiyeohre@gmail.com>
2023-02-27 16:52:08 -0800

    my fourth commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints all integers
+ * return: always 0 (sucess)
+ */
+
+int main(void)
+{
+	int d;
+	for (d = 0; d < 10; d++)
+		printf("%d", d);
+	printf("\n");
+	return (0);
+}
+
