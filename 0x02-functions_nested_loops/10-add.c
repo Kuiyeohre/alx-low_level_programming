0cb8d179be20b01c4757d9855b8e9341155196e0
kuiyekoghene ohre <kuiyeohre@gmail.com>
2023-03-04 12:46:07 -0800

    my 11th commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add:  adds two values
+ *
+ * @n1: first value
+ *
+ * @n2: second value
+ *
+ * return: integer value
+ *
+ */
+
+int add(int n1, int n2)
+{
+return (n1 + n2);
+}
