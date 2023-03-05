e8dce1739170a7d5bfbf2d77c1cda14a249df927
kuiyekoghene ohre <kuiyeohre@gmail.com>
2023-03-05 05:19:16 -0800

    my 3rd commit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * mul - multiplys characters.
+ *
+ * @a; character to be multiplied.
+ *
+ * @b: character to be multiplied.
+ *
+ * @c: solution.
+ *
+ * return :c
+ */
+int mul(int a, int b)
+{
+	int c = a * b;
+	return (c);
+}
