27300c87ac5bf3f23c460ae0b7fbe843b3355dfb
kuiyekoghene ohre <kuiyeohre@gmail.com>
2023-03-05 09:50:55 -0800

    my 10th commit

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * print_triangle - print triangle.
+ *
+ * @size: determines size of triangle.
+ *
+ * @x: dimension of traingle.
+ *
+ * @y: dimension of triangle.
+ *
+ * @x: dimension of triangle.
+ *
+ */
+
+void print_triangle(int size)
+{
+	int x, y, z;
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (x = 0; x < size; x++)
+		{
+		for  (y = size - x; y > 1; y--)
+		{
+		_putchar(32);
+		}
+		for (z = 0; z <= x; x++)
+		{
+		_putchar(35);
+		}
+		_putchar('\n');
+		}
+	}
+}
+	
