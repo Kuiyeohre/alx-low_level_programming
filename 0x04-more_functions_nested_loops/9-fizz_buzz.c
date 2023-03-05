1c99f9714e8b0ff51d3f4d8ffde532d1b033091f
kuiyekoghene ohre <kuiyeohre@gmail.com>
2023-03-05 09:09:43 -0800

    my 9th commit

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - entry point.
+ *
+ * @n: integer.
+ *
+ * return: always 0, success.
+ *
+ */
+
+int main(void)
+{
+	int n;
+	for (n = 1; n <= 100; n++)
+	{ if (n % 3 == 0 && n % 5 != 0)
+		{	
+			printf("Fizz ");
+		}
+		else if (n % 3 != 0 && n % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else if (n % 3 == 0 && n % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else
+		{
+			printf("%d ", n);
+		}
+		
+	}
+	printf("\n");
+	return (0);
+}
