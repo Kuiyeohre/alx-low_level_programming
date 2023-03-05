2f0df3672d4f704a1616483450a81cb81c49701b
kuiyekoghene ohre <kuiyeohre@gmail.com>
2023-03-04 17:59:11 -0800

    lkj

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,12 +2,14 @@
 
 /**
  * main - Prints the size of various types of files based on the computer it is complied on.
- * return: always (0)
+ *
+ * return: always (0).
+ *
  */
 
 int main(void)
 {
-	printf("size of a char: %zu bytes(s)\n", sizeof(char));
+	printf("size of a char: %zu byte(s)\n", sizeof(char));
 	printf("size of an int: %zu byte(s)\n", sizeof(int));
 	printf("size of a long int: %zu byte(s)\n", sizeof(long int));
 	printf("size of a long long int: %zu byte(s)\n", sizeof(long long int));
